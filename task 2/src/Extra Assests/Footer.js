41b2e6df172288ae8387ddb299eff2c73770bae9
Anujadesale <90961782+Anujadesale@users.noreply.github.com>
2021-10-25 20:51:34 +0530

    Add files via upload

---
+++ task 2/src/Extra Assests/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <>
+      <div className="footer">
+        <div class="contain">
+          <div class="col">
+            <h1>LETSGROWMORE</h1>
+            
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Footer;
